47705ad0215f32e61d60c7bf901e7fc182d7f614
Jeremy Banks <workjeremyb@gmail.com>
2024-01-22 07:47:14 -0800

    output list start

--- terraform/aws/enterprise-ad/outputs.tf
+++ terraform/aws/enterprise-ad/outputs.tf
@@ -6,6 +6,10 @@ output "ad_security_group_id_primary" {
   value =  aws_directory_service_directory.ad_primary.security_group_id
 }
 
-# output "ad_security_group_id_failover" {
-#   value =  data.aws_directory_service_directory.ad_failover.security_group_id
-# }
+output "ad_security_group_id_failover" {
+  value =  data.aws_directory_service_directory.ad_failover.security_group_id
+}
+
+output "ad_shared_directory_id" {
+  value = { for dir in aws_directory_service_shared_directory.ad_primary : dir.id => dir.shared_directory_id }
+}
