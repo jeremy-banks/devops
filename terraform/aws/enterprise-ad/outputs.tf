6fd1dbe02c5c5fd83224ff10c174ad3031ddf1f4
Jeremy Banks <workjeremyb@gmail.com>
2024-01-22 08:03:00 -0800

    working output for shared directory id map

--- terraform/aws/enterprise-ad/outputs.tf
+++ terraform/aws/enterprise-ad/outputs.tf
@@ -10,6 +10,22 @@ output "ad_security_group_id_failover" {
   value =  data.aws_directory_service_directory.ad_failover.security_group_id
 }
 
-output "ad_shared_directory_id" {
-  value = { for dir in aws_directory_service_shared_directory.ad_primary : dir.id => dir.shared_directory_id }
+output "ad_directory_id" {
+  value = aws_directory_service_directory.ad_primary.id
+}
+
+# output "foo" {
+#   value = [for k in keys(aws_directory_service_shared_directory.ad_primary) : element(split("/", k), 1)] #gives account numbers
+# }
+
+# output "bar" {
+#   value = values(aws_directory_service_shared_directory.ad_primary)[*].shared_directory_id #gives directory numbers
+# }
+
+output "ad_account_id_shared_directory_map" {
+  value = { 
+    for idx, key in keys(aws_directory_service_shared_directory.ad_primary) : 
+      element(split("/", key), 1) => 
+      values(aws_directory_service_shared_directory.ad_primary)[idx].shared_directory_id 
+  }
 }
