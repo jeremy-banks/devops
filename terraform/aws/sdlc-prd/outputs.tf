5f3676584ea106f792fc58c58909526b7bdff76b
Jeremy B <workjeremyb@gmail.com>
2024-10-19 15:45:41 -0700

    looks good for outputs but i need to unify the acm

--- terraform/aws/sdlc-prd/outputs.tf
+++ terraform/aws/sdlc-prd/outputs.tf
@@ -1,84 +1,73 @@
-# output "iam_role_eks_cluster" {
-#   description = "The ID of the VPC"
-#   value       = module.iam_role_eks_cluster.iam_role_arn
-# }
-
-# output "iam_role_eks_cluster_services" {
-#   description = "The ID of the VPC"
-#   value       = module.iam_role_eks_cluster_services_node.iam_role_arn
-# }
-
-# output "iam_role_eks_worker_node" {
-#   description = "The ID of the VPC"
-#   value       = module.iam_role_eks_worker_node.iam_role_arn
-# }
-
-output "primary_vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc_primary.vpc_id
+output "acm_arn_primary" {
+  description = "The ARN of the certificate"
+  value = {
+    for domain, cert in module.acm_wildcard_cert_primary : domain => cert.acm_certificate_arn
+  }
 }
 
-output "primary_public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc_primary.public_subnets
-}
-
-output "primary_private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc_primary.private_subnets
-}
-
-output "primary_nat_public_ips" {
-  description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value = aws_eip.vpc_nat_primary[*].public_ip
-}
-
-output "primary_security_group_id_main" {
-  description = "The ID of the security group created for general use"
-  value       = module.vpc_main_sg_primary.security_group_id
-}
-
-# output "primary_acm_arn" {
-#   description = "The ARN of the certificate"
-#   value       = module.acm_wildcard_cert_primary[*].acm_certificate_arn
-# }
-
-output "primary_kms_arn" {
+output "kms_arn_primary" {
   description = "The ARN of the key"
   value       = module.kms_primary.key_arn
 }
 
-output "failover_vpc_id" {
+output "vpc_id_primary" {
+  description = "The ID of the VPC"
+  value       = module.vpc_primary.vpc_id
+}
+
+output "vpc_nat_public_ips_primary" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value = aws_eip.vpc_nat_primary[*].public_ip
+}
+
+output "vpc_security_group_id_main_primary" {
+  description = "The ID of the security group created for general use"
+  value       = module.vpc_main_sg_primary.security_group_id
+}
+
+output "vpc_subnets_pub_primary" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc_primary.public_subnets
+}
+
+output "vpc_subnets_pvt_primary" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc_primary.private_subnets
+}
+
+output "acm_arn_failover" {
+  description = "The ARN of the certificate"
+  value = {
+    for domain, cert in module.acm_wildcard_cert_failover : domain => cert.acm_certificate_arn
+  }
+}
+
+output "kms_arn_failover" {
+  description = "The ARN of the key"
+  value       = module.kms_failover.key_arn
+}
+
+output "vpc_id_failover" {
   description = "The ID of the VPC"
   value       = module.vpc_failover.vpc_id
 }
 
-output "failover_public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc_failover.public_subnets
-}
-
-output "failover_private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc_failover.private_subnets
-}
-
-output "failover_nat_public_ips" {
+output "vpc_nat_public_ips_failover" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
   value = aws_eip.vpc_nat_failover[*].public_ip
 }
 
-output "failover_security_group_id_main" {
+output "vpc_security_group_id_main_failover" {
   description = "The ID of the security group created for general use"
   value       = module.vpc_main_sg_failover.security_group_id
 }
 
-output "failover_acm_arn" {
-  description = "The ARN of the certificate"
-  value       = [for cert in module.acm_wildcard_cert_failover : cert.acm_certificate_arn]
+output "vpc_subnets_pub_failover" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc_failover.public_subnets
 }
 
-output "failover_kms_arn" {
-  description = "The ARN of the key"
-  value       = module.kms_failover.key_arn
+output "vpc_subnets_pvt_failover" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc_failover.private_subnets
 }
