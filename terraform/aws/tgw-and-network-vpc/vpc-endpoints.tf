13e97c4c1bdbede35b6e5fb03d3a23fc2206c5a3
Jeremy B <workjeremyb@gmail.com>
2024-10-27 00:49:57 -0700

    need to fix endpoints

--- terraform/aws/tgw-and-network-vpc/vpc-endpoints.tf
+++ terraform/aws/tgw-and-network-vpc/vpc-endpoints.tf
@@ -1,81 +1,83 @@
-# module "vpc_endpoints_primary" {
-#   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-#   version = "5.13.0"
-#   providers = { aws = aws.network }
+module "vpc_endpoints_primary" {
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.13.0"
+  providers = { aws = aws.network }
 
-#   vpc_id             = module.vpc_primary.vpc_id
-#   security_group_ids = [module.vpc_main_sg_primary.security_group_id]
+  count = var.vpc_endpoints_enabled ? 1 : 0
 
-#   endpoints = {
-#     # dynamodb = {
-#     #   service             = "dynamodb"
-#     #   service_type        = "Interface"
-#     #   security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#     #   subnet_ids          = module.vpc_primary.private_subnets
-#     #   route_table_ids     = module.vpc_primary.private_route_table_ids
-#     #   tags                = { Name = "${local.resource_name_stub_primary}-dynamodb-endpoint" }
-#     # },
-#     # kms = {
-#     #   service             = "kms"
-#     #   service_type        = "Interface"
-#     #   private_dns_enabled = true
-#     #   security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#     #   subnet_ids          = module.vpc_primary.private_subnets
-#     #   route_table_ids     = module.vpc_primary.private_route_table_ids
-#     #   tags                = { Name = "${local.resource_name_stub_primary}-kms-endpoint" }
-#     # },
-#     # rds = {
-#     #   service             = "rds"
-#     #   private_dns_enabled = true
-#     #   security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#     #   subnet_ids          = module.vpc_primary.private_subnets
-#     #   tags                = { Name = "${local.resource_name_stub_primary}-rds-endpoint" }
-#     # },
-#     s3 = {
-#       service             = "s3"
-#       service_type        = "Interface"
-#       security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#       subnet_ids          = module.vpc_primary.private_subnets
-#       route_table_ids     = module.vpc_primary.private_route_table_ids
-#       tags                = { Name = "${local.resource_name_stub_primary}-s3-endpoint" }
-#     },
-#     # secretsmanager = {
-#     #   service             = "secretsmanager"
-#     #   service_type        = "Interface"
-#     #   private_dns_enabled = true
-#     #   security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#     #   subnet_ids          = module.vpc_primary.private_subnets
-#     #   route_table_ids     = module.vpc_primary.private_route_table_ids
-#     #   tags                = { Name = "${local.resource_name_stub_primary}-secretsmanager-endpoint" }
-#     # },
-#     # sns = {
-#     #   service             = "sns"
-#     #   service_type        = "Interface"
-#     #   private_dns_enabled = true
-#     #   security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#     #   subnet_ids          = module.vpc_primary.private_subnets
-#     #   route_table_ids     = module.vpc_primary.private_route_table_ids
-#     #   tags                = { Name = "${local.resource_name_stub_primary}-sns-endpoint" }
-#     # },
-#     # sqs = {
-#     #   service             = "sqs"
-#     #   service_type        = "Interface"
-#     #   private_dns_enabled = true
-#     #   security_group_ids  = [module.vpc_main_sg_primary.security_group_id]
-#     #   subnet_ids          = module.vpc_primary.private_subnets
-#     #   route_table_ids     = module.vpc_primary.private_route_table_ids
-#     #   tags                = { Name = "${local.resource_name_stub_primary}-sqs-endpoint" }
-#     # },
-#   }
-# }
+  vpc_id             = module.vpc_primary[0].vpc_id
+  security_group_ids = [module.vpc_main_sg_primary[0].security_group_id]
+
+  endpoints = {
+    # dynamodb = {
+    #   service             = "dynamodb"
+    #   service_type        = "Interface"
+    #   security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+    #   subnet_ids          = module.vpc_primary[0].private_subnets
+    #   route_table_ids     = module.vpc_primary[0].private_route_table_ids
+    #   tags                = { Name = "${local.resource_name_stub_primary}-dynamodb-endpoint" }
+    # },
+    # kms = {
+    #   service             = "kms"
+    #   service_type        = "Interface"
+    #   private_dns_enabled = true
+    #   security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+    #   subnet_ids          = module.vpc_primary[0].private_subnets
+    #   route_table_ids     = module.vpc_primary[0].private_route_table_ids
+    #   tags                = { Name = "${local.resource_name_stub_primary}-kms-endpoint" }
+    # },
+    # rds = {
+    #   service             = "rds"
+    #   private_dns_enabled = true
+    #   security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+    #   subnet_ids          = module.vpc_primary[0].private_subnets
+    #   tags                = { Name = "${local.resource_name_stub_primary}-rds-endpoint" }
+    # },
+    s3 = var.vpc_endpoint_s3_enabled ? {
+      service             = "s3"
+      service_type        = "Interface"
+      security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+      subnet_ids          = module.vpc_primary[0].private_subnets
+      route_table_ids     = module.vpc_primary[0].private_route_table_ids
+      tags                = { Name = "${local.resource_name_stub_primary}-s3-endpoint" }
+    } : null
+    # secretsmanager = {
+    #   service             = "secretsmanager"
+    #   service_type        = "Interface"
+    #   private_dns_enabled = true
+    #   security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+    #   subnet_ids          = module.vpc_primary[0].private_subnets
+    #   route_table_ids     = module.vpc_primary[0].private_route_table_ids
+    #   tags                = { Name = "${local.resource_name_stub_primary}-secretsmanager-endpoint" }
+    # },
+    # sns = {
+    #   service             = "sns"
+    #   service_type        = "Interface"
+    #   private_dns_enabled = true
+    #   security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+    #   subnet_ids          = module.vpc_primary[0].private_subnets
+    #   route_table_ids     = module.vpc_primary[0].private_route_table_ids
+    #   tags                = { Name = "${local.resource_name_stub_primary}-sns-endpoint" }
+    # },
+    # sqs = {
+    #   service             = "sqs"
+    #   service_type        = "Interface"
+    #   private_dns_enabled = true
+    #   security_group_ids  = [module.vpc_main_sg_primary[0].security_group_id]
+    #   subnet_ids          = module.vpc_primary[0].private_subnets
+    #   route_table_ids     = module.vpc_primary[0].private_route_table_ids
+    #   tags                = { Name = "${local.resource_name_stub_primary}-sqs-endpoint" }
+    # },
+  }
+}
 
 # module "vpc_endpoints_failover" {
 #   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
 #   version = "5.13.0"
 #   providers = { aws = aws.network_failover }
 
-#   vpc_id             = module.vpc_failover.vpc_id
-#   security_group_ids = [module.vpc_main_sg_failover.security_group_id]
+#   vpc_id             = module.vpc_failover[0].vpc_id
+#   security_group_ids = [module.vpc_main_sg_failover[0].security_group_id]
 
 #   endpoints = {
 #     s3 = {
@@ -83,9 +85,9 @@
 #       service             = "s3"
 #       service_type  = "Gateway"
 #       # private_dns_enabled = true
-#       # security_group_ids  = [module.vpc_main_sg_failover.security_group_id]
-#       subnet_ids = module.vpc_failover.private_subnets
-#       route_table_ids = flatten([module.vpc_failover.private_route_table_ids, module.vpc_failover.public_route_table_ids])
+#       # security_group_ids  = [module.vpc_main_sg_failover[0].security_group_id]
+#       subnet_ids = module.vpc_failover[0].private_subnets
+#       route_table_ids = flatten([module.vpc_failover[0].private_route_table_ids, module.vpc_failover[0].public_route_table_ids])
 #       tags                = { Name = "${local.resource_name_stub_failover}-s3-vpc-endpoint" }
 #     },
 #     rds = {
@@ -93,9 +95,9 @@
 #       service             = "rds"
 #       # service_type  = "Gateway"
 #       private_dns_enabled = true
-#       # security_group_ids  = [module.vpc_main_sg_failover.security_group_id]
-#       subnet_ids = module.vpc_failover.private_subnets
-#       # route_table_ids = flatten([module.vpc_failover.private_route_table_ids, module.vpc_failover.public_route_table_ids])
+#       # security_group_ids  = [module.vpc_main_sg_failover[0].security_group_id]
+#       subnet_ids = module.vpc_failover[0].private_subnets
+#       # route_table_ids = flatten([module.vpc_failover[0].private_route_table_ids, module.vpc_failover[0].public_route_table_ids])
 #       tags                = { Name = "${local.resource_name_stub_failover}-rds-endpoint" }
 #     },
 #     # dynamodb = {
