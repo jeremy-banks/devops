ea05501a6fbf885c55408e0efb3d275df5f2609e
Jeremy B <workjeremyb@gmail.com>
2024-10-26 23:57:09 -0700

    fixed tags for vpc and subnet

--- terraform/aws/tgw-and-network-vpc/vpc-ram-tags-identity.tf
+++ terraform/aws/tgw-and-network-vpc/vpc-ram-tags-identity.tf
@@ -105,77 +105,77 @@ resource "aws_ec2_tag" "vpc_identity_failover" {
 resource "aws_ec2_tag" "subnets_pub0_identity_failover" {
   provider    = aws.identity_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_infrastructure_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0}" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}0}" })) : 0
 
-  resource_id = module.vpc_primary[0].public_subnets[0]
-  key         = keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0" }))[count.index]
-  value       = merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0" })[keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].public_subnets[0]
+  key         = keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}0" }))[count.index]
+  value       = merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}0" })[keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pub1_identity_failover" {
   provider    = aws.identity_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_infrastructure_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1}" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}1}" })) : 0
 
-  resource_id = module.vpc_primary[0].public_subnets[1]
-  key         = keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1" }))[count.index]
-  value       = merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1" })[keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].public_subnets[1]
+  key         = keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}1" }))[count.index]
+  value       = merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}1" })[keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pub2_identity_failover" {
   provider    = aws.identity_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_infrastructure_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" })) : 0
+  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}2" })) : 0
 
-  resource_id = module.vpc_primary[0].public_subnets[2]
-  key         = keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" }))[count.index]
-  value       = merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" })[keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].public_subnets[2]
+  key         = keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}2" }))[count.index]
+  value       = merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}2" })[keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pvt0_identity_failover" {
   provider    = aws.identity_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_infrastructure_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}0" })) : 0
 
-  resource_id = module.vpc_primary[0].private_subnets[0]
-  key         = keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" }))[count.index]
-  value       = merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" })[keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].private_subnets[0]
+  key         = keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}0" }))[count.index]
+  value       = merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}0" })[keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pvt1_identity_failover" {
   provider    = aws.identity_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_infrastructure_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}1" })) : 0
 
-  resource_id = module.vpc_primary[0].private_subnets[1]
-  key         = keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" }))[count.index]
-  value       = merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" })[keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].private_subnets[1]
+  key         = keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}1" }))[count.index]
+  value       = merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}1" })[keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pvt2_identity_failover" {
   provider    = aws.identity_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_infrastructure_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" })) : 0
+  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}2" })) : 0
 
-  resource_id = module.vpc_primary[0].private_subnets[2]
-  key         = keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" }))[count.index]
-  value       = merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" })[keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].private_subnets[2]
+  key         = keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}2" }))[count.index]
+  value       = merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}2" })[keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}${count.index}" }))[count.index]]
 }
--- terraform/aws/tgw-and-network-vpc/vpc-ram-tags-log-archive.tf
+++ terraform/aws/tgw-and-network-vpc/vpc-ram-tags-log-archive.tf
@@ -1,7 +1,7 @@
 resource "aws_ec2_tag" "vpc_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = length(merge(local.vpc_tags_primary, local.default_tags, { "Name" = "${local.vpc_name_primary}" }))
@@ -14,7 +14,7 @@ resource "aws_ec2_tag" "vpc_log_archive" {
 resource "aws_ec2_tag" "subnets_pub0_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0}" }))
@@ -27,7 +27,7 @@ resource "aws_ec2_tag" "subnets_pub0_log_archive" {
 resource "aws_ec2_tag" "subnets_pub1_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1}" }))
@@ -40,7 +40,7 @@ resource "aws_ec2_tag" "subnets_pub1_log_archive" {
 resource "aws_ec2_tag" "subnets_pub2_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = var.vpc_five9s_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" })) : 0
@@ -53,7 +53,7 @@ resource "aws_ec2_tag" "subnets_pub2_log_archive" {
 resource "aws_ec2_tag" "subnets_pvt0_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" }))
@@ -66,7 +66,7 @@ resource "aws_ec2_tag" "subnets_pvt0_log_archive" {
 resource "aws_ec2_tag" "subnets_pvt1_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" }))
@@ -79,7 +79,7 @@ resource "aws_ec2_tag" "subnets_pvt1_log_archive" {
 resource "aws_ec2_tag" "subnets_pvt2_log_archive" {
   provider    = aws.log_archive
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
+    aws_ram_principal_association.network_primary_security_ou,
     aws_ram_resource_association.vpc_primary_subnets,
   ]
   count       = var.vpc_five9s_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" })) : 0
@@ -105,77 +105,77 @@ resource "aws_ec2_tag" "vpc_log_archive_failover" {
 resource "aws_ec2_tag" "subnets_pub0_log_archive_failover" {
   provider    = aws.log_archive_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_security_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0}" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}0}" })) : 0
 
-  resource_id = module.vpc_primary[0].public_subnets[0]
-  key         = keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0" }))[count.index]
-  value       = merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}0" })[keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].public_subnets[0]
+  key         = keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}0" }))[count.index]
+  value       = merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}0" })[keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pub1_log_archive_failover" {
   provider    = aws.log_archive_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_security_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1}" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}1}" })) : 0
 
-  resource_id = module.vpc_primary[0].public_subnets[1]
-  key         = keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1" }))[count.index]
-  value       = merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}1" })[keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].public_subnets[1]
+  key         = keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}1" }))[count.index]
+  value       = merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}1" })[keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pub2_log_archive_failover" {
   provider    = aws.log_archive_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_security_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" })) : 0
+  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}2" })) : 0
 
-  resource_id = module.vpc_primary[0].public_subnets[2]
-  key         = keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" }))[count.index]
-  value       = merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}2" })[keys(merge(local.subnet_pub_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].public_subnets[2]
+  key         = keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}2" }))[count.index]
+  value       = merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}2" })[keys(merge(local.subnet_pub_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pub_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pvt0_log_archive_failover" {
   provider    = aws.log_archive_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_security_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}0" })) : 0
 
-  resource_id = module.vpc_primary[0].private_subnets[0]
-  key         = keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" }))[count.index]
-  value       = merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}0" })[keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].private_subnets[0]
+  key         = keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}0" }))[count.index]
+  value       = merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}0" })[keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pvt1_log_archive_failover" {
   provider    = aws.log_archive_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_security_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" })) : 0
+  count       = var.vpc_failover_enabled ? length(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}1" })) : 0
 
-  resource_id = module.vpc_primary[0].private_subnets[1]
-  key         = keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" }))[count.index]
-  value       = merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}1" })[keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].private_subnets[1]
+  key         = keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}1" }))[count.index]
+  value       = merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}1" })[keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}${count.index}" }))[count.index]]
 }
 
 resource "aws_ec2_tag" "subnets_pvt2_log_archive_failover" {
   provider    = aws.log_archive_failover
   depends_on  = [
-    aws_ram_principal_association.network_primary_infrastructure_ou,
-    aws_ram_resource_association.vpc_primary_subnets,
+    aws_ram_principal_association.network_failover_security_ou,
+    aws_ram_resource_association.vpc_failover_subnets,
   ]
-  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" })) : 0
+  count       = var.vpc_failover_enabled && var.vpc_five9s_enabled ? length(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}2" })) : 0
 
-  resource_id = module.vpc_primary[0].private_subnets[2]
-  key         = keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" }))[count.index]
-  value       = merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}2" })[keys(merge(local.subnet_pvt_tags_primary, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_primary}${count.index}" }))[count.index]]
+  resource_id = module.vpc_failover[0].private_subnets[2]
+  key         = keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}2" }))[count.index]
+  value       = merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}2" })[keys(merge(local.subnet_pvt_tags_failover, local.default_tags, { "Name" = "${local.vpc_subnet_pvt_name_failover}${count.index}" }))[count.index]]
 }
