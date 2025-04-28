988f83bac9bb1aebda8bd59c177933209548aaa3
jeremy-banks <workjeremyb@gmail.com>
2025-04-28 05:59:03 -0700

    working

--- terraform/aws/tests-networking/outbound-pub.tf
+++ terraform/aws/tests-networking/outbound-pub.tf
@@ -18,6 +18,17 @@ data "aws_subnets" "outbound_pub_primary" {
   tags = { Name = "*-pub-*" }
 }
 
+data "aws_subnets" "outbound_tgw_primary" {
+  provider = aws.networking_prd
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.outbound_primary.id]
+  }
+
+  tags = { Name = "*-tgw-*" }
+}
+
 data "aws_security_group" "outbound_primary" {
   provider = aws.networking_prd
 
@@ -33,36 +44,11 @@ data "aws_security_group" "outbound_primary" {
 #   value = data.aws_subnets.spoke_a_pub_primary.ids
 # }
 
-data "template_file" "outbound_pub_userdata_primary" {
-  template = <<-EOF
-    #!/bin/bash
-    # yum update -y
-    # yum install -y awslogs
+# data "template_file" "outbound_pub_userdata_primary" {
+#   template = <<-EOF
 
-    # cat > /etc/awslogs/awslogs.conf <<CONFIG
-    # [general]
-    # state_file = /var/lib/awslogs/agent-state
-
-    # [/var/log/cloud-init.log]
-    # file = /var/log/cloud-init.log
-    # log_group_name = /ec2/user-data-output
-    # log_stream_name = ${var.this_slug}-outbound-pub
-    # datetime_format = %Y-%m-%dT%H:%M:%S
-    # CONFIG
-
-    # sed -i "s/region = .*/region = ${var.region.primary}/" /etc/awslogs/awscli.conf
-
-    # systemctl enable awslogsd
-    # systemctl start awslogsd
-
-    # ping google.com
-
-    # which aws && shutdown -h now
-    THIS_INSTANCE_ID=$(ec2-metadata --instance-id | awk '{print $2}')
-
-    aws ec2 terminate-instances --instance-ids $THIS_INSTANCE_ID --region ${var.region.primary}
-  EOF
-}
+#   EOF
+# }
 
 module "outbound_pub_primary" {
   source    = "terraform-aws-modules/ec2-instance/aws"
@@ -85,5 +71,35 @@ module "outbound_pub_primary" {
   create_iam_instance_profile = true
   iam_role_policies           = { PowerUserAccess = "arn:aws:iam::aws:policy/PowerUserAccess" }
 
-  user_data_base64 = base64encode(data.template_file.outbound_pub_userdata_primary.rendered)
+  user_data = templatefile("user_data.tftpl", {
+    instance_name   = "${var.this_slug}-outbound-pub",
+    instance_region = var.region.primary,
+  })
+}
+
+module "outbound_pvt_primary" {
+  source    = "terraform-aws-modules/ec2-instance/aws"
+  version   = "5.8.0"
+  providers = { aws = aws.networking_prd }
+
+  #   count = var.azs_used
+
+  name = "${var.this_slug}-outbound-pvt"
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t4g.nano"
+
+  subnet_id              = element(data.aws_subnets.outbound_tgw_primary.ids, 0)
+  vpc_security_group_ids = [data.aws_security_group.outbound_primary.id]
+
+  # associate_public_ip_address = true
+  key_name = "me"
+
+  create_iam_instance_profile = true
+  iam_role_policies           = { PowerUserAccess = "arn:aws:iam::aws:policy/PowerUserAccess" }
+
+  user_data = templatefile("user_data.tftpl", {
+    instance_name   = "${var.this_slug}-outbound-pvt",
+    instance_region = var.region.primary,
+  })
 }
