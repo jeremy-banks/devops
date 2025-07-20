8cb0a29cd554f5a4db31cd13107a14fb3474aad9
jeremy-banks <jeremybankstech@gmail.com>
2025-07-19 19:19:48 -0700

    updating outputs for eks

--- terraform/aws/networking/outputs.tf
+++ terraform/aws/networking/outputs.tf
@@ -1,10 +1,3 @@
-output "vpc_inbound_failover" {
-  value = {
-    subnets_public = module.vpc_inbound_failover.*.public_subnets
-    vpc_id = module.vpc_inbound_failover.*.vpc_id
-  }
-}
-
 output "vpc_inbound_primary" {
   value = {
     subnets_public = module.vpc_inbound_primary.*.public_subnets
@@ -12,13 +5,20 @@ output "vpc_inbound_primary" {
   }
 }
 
-output "vpc_outbound_failover" {
-  value = {
-    public_ips = aws_eip.vpc_outbound_failover_nat.*.public_ip
+output "vpc_outbound_primary" {
+  value = {    public_ips = aws_eip.vpc_outbound_primary_nat.*.public_ip
   }
 }
 
-output "vpc_outbound_primary" {
-  value = {    public_ips = aws_eip.vpc_outbound_primary_nat.*.public_ip
+output "vpc_inbound_failover" {
+  value = {
+    subnets_public = module.vpc_inbound_failover.*.public_subnets
+    vpc_id = module.vpc_inbound_failover.*.vpc_id
+  }
+}
+
+output "vpc_outbound_failover" {
+  value = {
+    public_ips = aws_eip.vpc_outbound_failover_nat.*.public_ip
   }
 }
