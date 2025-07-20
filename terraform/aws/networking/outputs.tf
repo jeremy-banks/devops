2d403ed7abb959f2254127808635787e15e81907
jeremy-banks <jeremybankstech@gmail.com>
2025-07-19 19:19:12 -0700

    updating outputs for eks

--- terraform/aws/networking/outputs.tf
+++ terraform/aws/networking/outputs.tf
@@ -1,35 +1,24 @@
-# output "vpc_primary" {
-#   value = {
-#     subnets_private = module.vpc_primary.*.private_subnets
-#     subnets_public = module.vpc_primary.*.public_subnets
-#     vpc_id = module.vpc_primary.*.vpc_id
-#   }
-# }
+output "vpc_inbound_failover" {
+  value = {
+    subnets_public = module.vpc_inbound_failover.*.public_subnets
+    vpc_id = module.vpc_inbound_failover.*.vpc_id
+  }
+}
 
-# output "vpc_nat_public_ips_primary" {
-#   description = "List of public Elastic IPs created for AWS NAT Gateway"
-#   value = aws_eip.vpc_nat_primary.*.public_ip
-# }
+output "vpc_inbound_primary" {
+  value = {
+    subnets_public = module.vpc_inbound_primary.*.public_subnets
+    vpc_id = module.vpc_inbound_primary.vpc_id
+  }
+}
 
-# output "vpc_security_group_id_main_primary" {
-#   description = "The ID of the security group created for general use"
-#   value       = module.vpc_main_sg_primary.*.security_group_id
-# }
+output "vpc_outbound_failover" {
+  value = {
+    public_ips = aws_eip.vpc_outbound_failover_nat.*.public_ip
+  }
+}
 
-# output "vpc_failover" {
-#   value = {
-#     subnets_private = module.vpc_failover.*.private_subnets
-#     subnets_public = module.vpc_failover.*.public_subnets
-#     vpc_id = module.vpc_failover.*.vpc_id
-#  }
-# }
-
-# output "vpc_nat_public_ips_failover" {
-#   description = "List of public Elastic IPs created for AWS NAT Gateway"
-#   value = aws_eip.vpc_nat_failover.*.public_ip
-# }
-
-# output "vpc_security_group_id_main_failover" {
-#   description = "The ID of the security group created for general use"
-#   value       = module.vpc_main_sg_failover.*.security_group_id
-# }
+output "vpc_outbound_primary" {
+  value = {    public_ips = aws_eip.vpc_outbound_primary_nat.*.public_ip
+  }
+}
