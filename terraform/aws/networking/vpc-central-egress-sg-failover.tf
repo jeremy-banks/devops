f828bce5e46e3c66ac3951bb727758d0f98397e6
jeremy-banks <jeremybankstech@gmail.com>
2025-07-29 13:33:02 -0700

    moving security group

---
+++ terraform/aws/networking/vpc-central-egress-sg-failover.tf
@@ -0,0 +1,22 @@
+module "sg_central_egress_main_failover" {
+  source    = "terraform-aws-modules/security-group/aws"
+  version   = "5.3.0"
+  providers = { aws = aws.networking_prd_failover }
+
+  count = var.create_failover_region ? 1 : 0
+
+  name        = "${local.resource_name_stub_failover}-${var.this_slug}-central-egress-main-sg"
+  description = "${local.resource_name_stub_failover}-${var.this_slug}-central-egress-main-sg"
+  vpc_id      = module.vpc_central_egress_failover[0].vpc_id
+
+  ingress_with_self = [{ rule = "all-all" }]
+  egress_with_self  = [{ rule = "all-all" }]
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["all-all"]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+
+  tags = { Name = "${local.resource_name_stub_failover}-${var.this_slug}-central-egress-main-sg" }
+}
---
+++ terraform/aws/networking/vpc-central-ingress-sg-primary.tf
@@ -0,0 +1,20 @@
+module "sg_central_ingress_main_primary" {
+  source    = "terraform-aws-modules/security-group/aws"
+  version   = "5.3.0"
+  providers = { aws = aws.networking_prd }
+
+  name        = "${local.resource_name_stub_primary}-${var.this_slug}-central-ingress-main-sg"
+  description = "${local.resource_name_stub_primary}-${var.this_slug}-central-ingress-main-sg"
+  vpc_id      = module.vpc_central_ingress_primary.vpc_id
+
+  ingress_with_self = [{ rule = "all-all" }]
+  egress_with_self  = [{ rule = "all-all" }]
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["all-all"]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+
+  tags = { Name = "${local.resource_name_stub_primary}-${var.this_slug}-central-ingress-main-sg" }
+}
