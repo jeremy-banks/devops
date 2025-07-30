dd10d81fa78b7d229f53d45b50b13286d91dc8a5
jeremy-banks <jeremybankstech@gmail.com>
2025-07-30 11:29:43 -0700

    standardizing outputs

--- terraform/aws/management-account/outputs.tf
+++ terraform/aws/management-account/outputs.tf
@@ -1,29 +1,20 @@
-output "tfstate_region" {
-  value = var.region_primary.full
-}
+output "tfstate_region" { value = var.region_primary.full }
 
-output "tfstate_s3_bucket_name" {
-  value = module.s3_tfstate_backend_primary.s3_bucket_id
-}
+output "tfstate_s3_bucket_name" { value = module.s3_tfstate_backend_primary.s3_bucket_id }
 
-output "tfstate_kms_arn" {
-  value = module.kms_tfstate_backend_primary.key_arn
-}
+output "tfstate_kms_arn" { value = module.kms_tfstate_backend_primary.key_arn }
 
 output "iam_user_admin_access_key" {
-  description = "admin access key"
-  value       = module.iam_user_admin.iam_access_key_id
-  sensitive   = true
+  value     = module.iam_user_admin.iam_access_key_id
+  sensitive = true
 }
 
 output "iam_user_admin_access_secret" {
-  description = "admin access secret"
-  value       = module.iam_user_admin.iam_access_key_secret
-  sensitive   = true
+  value     = module.iam_user_admin.iam_access_key_secret
+  sensitive = true
 }
 
 output "iam_user_breakglass_credentials" {
-  description = "Map of IAM usernames and passwords"
   value = { for idx, user in module.iam_user_breakglass :
     user.iam_user_name => user.iam_user_login_profile_password
   }
