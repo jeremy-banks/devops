2cead5aeecc006197d59fe491b891265ef818012
jeremy-banks <jeremybankstech@gmail.com>
2025-08-16 16:18:11 -0700

    idk

---
+++ terraform/aws/iam/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    profile      = "superadmin"
+    region       = "tfstate_region"
+    bucket       = "tfstate_s3_bucket_name"
+    key          = "superadmin/iam"
+    use_lockfile = true
+    insecure     = false
+    encrypt      = true
+    kms_key_id   = "tfstate_kms_arn"
+  }
+}
