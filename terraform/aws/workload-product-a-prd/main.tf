6d6df2e30c0fad1f7de21075c1b14626d6e98556
jeremy-banks <jeremybankstech@gmail.com>
2025-08-19 17:40:03 -0700

    everything but r53 looks like it will succeed

--- terraform/aws/workload-product-a-prd/main.tf
+++ terraform/aws/workload-product-a-prd/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   alias   = "this"
   profile = var.cli_profile_name
   region  = var.region_primary.full
-  assume_role { role_arn = "arn:aws:iam::${var.account_id.workload_product_a_prd}:role/${var.provider_role_name}" }
+  assume_role { role_arn = "arn:aws:iam::${var.account_id.product_a_prd}:role/${var.provider_role_name}" }
   default_tags { tags = local.default_tags_map }
 }
 
@@ -10,7 +10,7 @@ provider "aws" {
   alias   = "this_failover"
   profile = var.cli_profile_name
   region  = var.region_failover.full
-  assume_role { role_arn = "arn:aws:iam::${var.account_id.workload_product_a_prd}:role/${var.provider_role_name}" }
+  assume_role { role_arn = "arn:aws:iam::${var.account_id.product_a_prd}:role/${var.provider_role_name}" }
   default_tags { tags = local.default_tags_map }
 }
 
@@ -83,7 +83,7 @@ data "aws_ec2_transit_gateway_vpc_attachment" "tgw_post_central_inspection_prima
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_primary}-network-tgw-attach-central-inspection-vpc"]
+    values = ["vpc-central-inspection-tgw-attach-primary"]
   }
 
   filter {
@@ -97,7 +97,7 @@ data "aws_ec2_transit_gateway_route_table" "tgw_pre_central_inspection_primary" 
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_primary}-network-tgw-pre-inspection"]
+    values = ["tgw-pre-inspection-primary"]
   }
 
   filter {
@@ -111,7 +111,7 @@ data "aws_ec2_transit_gateway_route_table" "tgw_post_central_inspection_primary"
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_primary}-network-tgw-post-inspection"]
+    values = ["tgw-post-inspection-primary"]
   }
 
   filter {
@@ -127,7 +127,7 @@ data "aws_ec2_transit_gateway_peering_attachment" "tgw_peer_primary" {
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_primary}-network-tgw-peer-accepter"]
+    values = ["tgw-peer-accepter-primary"]
   }
 
   filter {
@@ -143,7 +143,7 @@ data "aws_ec2_transit_gateway_vpc_attachment" "tgw_post_inspection_failover" {
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_failover}-network-tgw-attach-central-inspection-vpc"]
+    values = ["vpc-central-inspection-tgw-attach-failover"]
   }
 
   filter {
@@ -159,7 +159,7 @@ data "aws_ec2_transit_gateway_route_table" "tgw_pre_inspection_failover" {
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_failover}-network-tgw-pre-inspection"]
+    values = ["tgw-pre-inspection-failover"]
   }
 
   filter {
@@ -175,7 +175,7 @@ data "aws_ec2_transit_gateway_route_table" "tgw_post_inspection_failover" {
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_failover}-network-tgw-post-inspection"]
+    values = ["tgw-post-inspection-failover"]
   }
 
   filter {
@@ -191,7 +191,7 @@ data "aws_ec2_transit_gateway_peering_attachment" "tgw_peer_failover" {
 
   filter {
     name   = "tag:Name"
-    values = ["${local.resource_name_failover}-network-tgw-peer-requester"]
+    values = ["tgw-peer-requester-failover"]
   }
 
   filter {
@@ -201,6 +201,6 @@ data "aws_ec2_transit_gateway_peering_attachment" "tgw_peer_failover" {
 }
 
 locals {
-  vpc_cidr_primary  = var.vpc_cidr_infrastructure.workload_product_a_prd_primary
-  vpc_cidr_failover = var.vpc_cidr_infrastructure.workload_product_a_prd_failover
+  vpc_cidr_primary  = var.vpc_cidr_infrastructure.product_a_prd_primary
+  vpc_cidr_failover = var.vpc_cidr_infrastructure.product_a_prd_failover
 }
