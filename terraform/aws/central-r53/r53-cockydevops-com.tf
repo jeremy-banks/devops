201a27d7f22e9687912647cfca25040f57dfbc25
jeremy-banks <jeremybankstech@gmail.com>
2025-08-24 17:08:50 -0700

    close to what i need already

--- terraform/aws/central-r53/r53-cockydevops-com.tf
+++ terraform/aws/central-r53/r53-cockydevops-com.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_zone" "cockydevops_com_central" {
+resource "aws_route53_zone" "cockydevops_com" {
   provider = aws.network_prd
 
   name = "cockydevops.com"
@@ -7,63 +7,63 @@ resource "aws_route53_zone" "cockydevops_com_central" {
 resource "aws_route53_zone" "cockydevops_com_prd" {
   provider = aws.network_prd
 
-  name = "prd.${aws_route53_zone.cockydevops_com_central.name}"
+  name = "prd.${aws_route53_zone.cockydevops_com.name}"
 }
 
-resource "aws_route53_record" "prd_ns" {
+resource "aws_route53_record" "cockydevops_com_prd_ns" {
   provider = aws.network_prd
 
-  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  zone_id  = aws_route53_zone.cockydevops_com.zone_id
   name     = aws_route53_zone.cockydevops_com_prd.name
   type     = "NS"
-  ttl      = 300
+  ttl      = 86400
   records  = aws_route53_zone.cockydevops_com_prd.name_servers
 }
 
 resource "aws_route53_zone" "cockydevops_com_stg" {
   provider = aws.network_prd
 
-  name = "stg.${aws_route53_zone.cockydevops_com_central.name}"
+  name = "stg.${aws_route53_zone.cockydevops_com.name}"
 }
 
-resource "aws_route53_record" "stg_ns" {
+resource "aws_route53_record" "cockydevops_com_stg_ns" {
   provider = aws.network_prd
 
-  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  zone_id  = aws_route53_zone.cockydevops_com.zone_id
   name     = aws_route53_zone.cockydevops_com_stg.name
   type     = "NS"
-  ttl      = 300
+  ttl      = 86400
   records  = aws_route53_zone.cockydevops_com_stg.name_servers
 }
 
 resource "aws_route53_zone" "cockydevops_com_tst" {
   provider = aws.network_prd
 
-  name = "tst.${aws_route53_zone.cockydevops_com_central.name}"
+  name = "tst.${aws_route53_zone.cockydevops_com.name}"
 }
 
-resource "aws_route53_record" "tst_ns" {
+resource "aws_route53_record" "cockydevops_com_tst_ns" {
   provider = aws.network_prd
 
-  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  zone_id  = aws_route53_zone.cockydevops_com.zone_id
   name     = aws_route53_zone.cockydevops_com_tst.name
   type     = "NS"
-  ttl      = 300
+  ttl      = 86400
   records  = aws_route53_zone.cockydevops_com_tst.name_servers
 }
 
 resource "aws_route53_zone" "cockydevops_com_dev" {
   provider = aws.network_prd
 
-  name = "dev.${aws_route53_zone.cockydevops_com_central.name}"
+  name = "dev.${aws_route53_zone.cockydevops_com.name}"
 }
 
-resource "aws_route53_record" "dev_ns" {
+resource "aws_route53_record" "cockydevops_com_dev_ns" {
   provider = aws.network_prd
 
-  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  zone_id  = aws_route53_zone.cockydevops_com.zone_id
   name     = aws_route53_zone.cockydevops_com_dev.name
   type     = "NS"
-  ttl      = 300
+  ttl      = 86400
   records  = aws_route53_zone.cockydevops_com_dev.name_servers
 }
