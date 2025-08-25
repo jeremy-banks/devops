fcd37a94d4ccf8a149bb1ac8215c5854ee73c12e
jeremy-banks <jeremybankstech@gmail.com>
2025-08-24 17:00:23 -0700

    looks good time to test

--- terraform/aws/central-r53/r53-cockydevops-com.tf
+++ terraform/aws/central-r53/r53-cockydevops-com.tf
@@ -10,10 +10,30 @@ resource "aws_route53_zone" "cockydevops_com_prd" {
   name = "prd.${aws_route53_zone.cockydevops_com_central.name}"
 }
 
+resource "aws_route53_record" "prd_ns" {
+  provider = aws.network_prd
+
+  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  name     = aws_route53_zone.cockydevops_com_prd.name
+  type     = "NS"
+  ttl      = 300
+  records  = aws_route53_zone.cockydevops_com_prd.name_servers
+}
+
 resource "aws_route53_zone" "cockydevops_com_stg" {
   provider = aws.network_prd
 
   name = "stg.${aws_route53_zone.cockydevops_com_central.name}"
+}
+
+resource "aws_route53_record" "stg_ns" {
+  provider = aws.network_prd
+
+  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  name     = aws_route53_zone.cockydevops_com_stg.name
+  type     = "NS"
+  ttl      = 300
+  records  = aws_route53_zone.cockydevops_com_stg.name_servers
 }
 
 resource "aws_route53_zone" "cockydevops_com_tst" {
@@ -22,8 +42,28 @@ resource "aws_route53_zone" "cockydevops_com_tst" {
   name = "tst.${aws_route53_zone.cockydevops_com_central.name}"
 }
 
+resource "aws_route53_record" "tst_ns" {
+  provider = aws.network_prd
+
+  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  name     = aws_route53_zone.cockydevops_com_tst.name
+  type     = "NS"
+  ttl      = 300
+  records  = aws_route53_zone.cockydevops_com_tst.name_servers
+}
+
 resource "aws_route53_zone" "cockydevops_com_dev" {
   provider = aws.network_prd
 
   name = "dev.${aws_route53_zone.cockydevops_com_central.name}"
+}
+
+resource "aws_route53_record" "dev_ns" {
+  provider = aws.network_prd
+
+  zone_id  = aws_route53_zone.cockydevops_com_central.zone_id
+  name     = aws_route53_zone.cockydevops_com_dev.name
+  type     = "NS"
+  ttl      = 300
+  records  = aws_route53_zone.cockydevops_com_dev.name_servers
 }
--- terraform/aws/central-r53/terraform.tfvars
+++ terraform/aws/central-r53/terraform.tfvars
@@ -4,4 +4,4 @@ provider_role_name = "superadmin"
 
 environment = "prd"
 
-this_slug = "central-backup"
+this_slug = "central-r53"
