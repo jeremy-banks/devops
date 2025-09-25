8fc0a4e991113fb5ec4cc2069dba0160351817f1
jeremy-banks <jeremybankstech@gmail.com>
2025-09-24 18:07:12 -0700

    initial work for sdlc

---
+++ terraform/aws/workload-sdlc-prd/s3-iam.tf
@@ -0,0 +1,76 @@
+data "aws_iam_policy_document" "s3_crr" {
+  provider = aws.this
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionTagging",
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+      "s3:ReplicateDelete",
+      "s3:ReplicateObject",
+      "s3:ReplicateTags",
+    ]
+    resources = [
+      "arn:aws:s3:::${local.resource_name_full_unique.primary}",
+      "arn:aws:s3:::${local.resource_name_full_unique.primary}/*",
+      "arn:aws:s3:::${local.resource_name_full_unique.failover}",
+      "arn:aws:s3:::${local.resource_name_full_unique.failover}/*",
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+    ]
+    resources = [
+      module.kms_primary.key_arn,
+      module.kms_failover.key_arn,
+    ]
+  }
+}
+
+module "iam_policy_s3_crr" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version   = "6.1.2"
+  providers = { aws = aws.this }
+
+  name = "${local.resource_name.primary}-crr"
+
+  policy = data.aws_iam_policy_document.s3_crr.json
+}
+
+module "iam_role_s3_crr" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version   = "6.1.2"
+  providers = { aws = aws.this }
+
+  name            = "${local.resource_name.primary}-crr"
+  use_name_prefix = false
+
+  trust_policy_permissions = {
+    TrustRoleAndServiceToAssume = {
+      principals = [
+        {
+          type = "Service"
+          identifiers = [
+            "s3.amazonaws.com",
+            "batchoperations.s3.amazonaws.com",
+          ]
+        }
+      ]
+      actions = [
+        "sts:AssumeRole",
+        "sts:TagSession",
+      ]
+    }
+  }
+
+  policies = {
+    replicate = module.iam_policy_s3_crr.arn,
+  }
+}
---
+++ terraform/aws/workload-sdlc-prd/vpc-tgw-primary.tf
@@ -0,0 +1,41 @@
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_workload_product_a_to_tgw_primary" {
+  provider = aws.this
+
+  subnet_ids         = module.vpc_primary.intra_subnets
+  transit_gateway_id = data.aws_ec2_transit_gateway.tgw_primary.id
+  vpc_id             = module.vpc_primary.vpc_id
+
+  appliance_mode_support             = "enable"
+  dns_support                        = "enable"
+  security_group_referencing_support = "enable"
+
+  # transit_gateway_default_route_table_association = false
+  # transit_gateway_default_route_table_propagation = false
+
+  tags = { Name = "${local.resource_name.primary}-tgw-attach" }
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "vpc_workload_product_a_to_tgw_primary" {
+  provider = aws.network_prd
+
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_workload_product_a_to_tgw_primary.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_pre_central_inspection_primary.id
+}
+
+resource "aws_ec2_transit_gateway_route" "post_central_inspection_workload_product_a_primary_to_primary" {
+  provider = aws.network_prd
+
+  destination_cidr_block         = local.vpc_cidr_primary
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_workload_product_a_to_tgw_primary.id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_post_central_inspection_primary.id
+}
+
+resource "aws_ec2_transit_gateway_route" "post_central_inspection_workload_product_a_primary_to_failover" {
+  provider = aws.network_prd
+
+  count = var.create_failover_region ? 1 : 0
+
+  destination_cidr_block         = local.vpc_cidr_failover
+  transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_peering_attachment.tgw_peer_failover[0].id
+  transit_gateway_route_table_id = data.aws_ec2_transit_gateway_route_table.tgw_post_central_inspection_primary.id
+}
